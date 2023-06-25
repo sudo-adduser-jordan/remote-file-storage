9063d3521b64eb8805c42787bae3b4a93e622363
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-25 10:29:12 -0700

    add env config

--- database/configs/env.go
+++ database/configs/env.go
@@ -1,1 +1,20 @@
 package configs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Env func to get env value
+func Env(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
