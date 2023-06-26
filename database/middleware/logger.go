1dbbbcad7d7284da4a8d38ff8067acf1acfc0dcc
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-25 23:02:25 -0700

    create middleware logger

--- database/middleware/logger.go
+++ database/middleware/logger.go
@@ -1,1 +1,28 @@
 package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// Logging logs all requests with its path and the time it took to process
+func Logging() Middleware {
+
+	// Create a new Middleware
+	return func(f http.HandlerFunc) http.HandlerFunc {
+
+		// Define the http.HandlerFunc
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			// Do middleware things
+			start := time.Now()
+			defer func() { log.Println(r.URL.Path, time.Since(start)) }()
+
+			// Call the next middleware/handler in chain
+			f(w, r)
+		}
+	}
+}
