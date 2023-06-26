f8f2e386397741eff2edd25402c09eb05bb7ef5f
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-25 22:59:16 -0700

    setup routes

--- database/handlers/handlers.go
+++ database/handlers/handlers.go
@@ -1,1 +1,23 @@
 package handlers
+
+import "net/http"
+
+// Login
+func Login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create Account
+func Register(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Upload
+func Upload(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Download
+func Download(w http.ResponseWriter, r *http.Request) {
+
+}
--- database/routes/routes.go
+++ database/routes/routes.go
@@ -1,1 +1,23 @@
 package routes
+
+import (
+	"main/handlers"
+	"net/http"
+)
+
+func SetupRoutes() {
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./static")))
+	mux.HandleFunc("/login", handlers.Login)
+	mux.HandleFunc("/upload", handlers.Upload)
+	mux.HandleFunc("/download", handlers.Download)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		// ErrorLog:  nil,
+		// TLSConfig: nil,
+	}
+	server.ListenAndServe()
+}
