9b4cb56b9624a0e87fa7e9914b6a7956c2fd447c
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-07-06 16:27:37 -0700

    dev batch file

---
+++ start-project.bat
@@ -0,0 +1,10 @@
+call code C:\Users\user1\Desktop\GitHub\Remote-File-Storage
+wt -d C:\Users\user1\Desktop\GitHub\Remote-File-Storage\website Powershell npm run dev
+start firefox "http://localhost:3000"
+
+
+docker start postgres-container
+start /d "C:/Users/user1/AppData/Local/Programs/pgAdmin 4/v7/runtime/" pgadmin4.exe
+cd C:\Users\user1\Desktop\GitHub\Remote-File-Storage\database
+go build cmd/main.go
+wt -d C:\Users\user1\Desktop\GitHub\Remote-File-Storage\database Powershell ./main.exe 
